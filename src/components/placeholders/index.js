d3dc61b005a4638c5a48dfd01b1f07943f7486de
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2020-01-20 02:07:21 +0500

    added new component for placeholders

---
+++ src/components/placeholders/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Grid, Placeholder} from 'semantic-ui-react';
+
+export const placeholderRestaurants = () => (
+    <React.Fragment>
+                <Grid.Column>
+                <Placeholder>
+                        <Placeholder.Image rectangular />
+                        <Placeholder.Header>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        </Placeholder.Header>
+                    </Placeholder>
+            </Grid.Column>
+            <Grid.Column>
+                <Placeholder>
+                        <Placeholder.Image rectangular />
+                        <Placeholder.Header>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        </Placeholder.Header>
+                    </Placeholder>
+            </Grid.Column>
+            <Grid.Column>
+                <Placeholder>
+                        <Placeholder.Image rectangular />
+                        <Placeholder.Header>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        </Placeholder.Header>
+                    </Placeholder>
+            </Grid.Column>
+            <Grid.Column>
+                <Placeholder>
+                        <Placeholder.Image rectangular />
+                        <Placeholder.Header>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        </Placeholder.Header>
+                    </Placeholder>
+            </Grid.Column>
+            <Grid.Column>
+                <Placeholder>
+                        <Placeholder.Image rectangular />
+                        <Placeholder.Header>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        </Placeholder.Header>
+                    </Placeholder>
+            </Grid.Column>
+            <Grid.Column>
+                <Placeholder>
+                        <Placeholder.Image rectangular />
+                        <Placeholder.Header>
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        <Placeholder.Line />
+                        </Placeholder.Header>
+                    </Placeholder>
+            </Grid.Column>
+    </React.Fragment>
+);
