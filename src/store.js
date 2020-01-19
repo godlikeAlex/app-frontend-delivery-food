3f3016096f8a9a1d6664de422ff13759ba96e8f0
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2020-01-20 02:09:49 +0500

    added redux devtols

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {locationReducer} from './reducer';
+import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(locationReducer)
+const store = createStore(locationReducer, composeWithDevTools(applyMiddleware(logger)))
 
 export default store;
