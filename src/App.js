2a369b0d9f621cb1ace1e15041fa49ddeea9b11c
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2020-02-01 00:10:07 +0500

    added notifications

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css'
 import Routes from './Routes';
+import { ToastProvider } from 'react-toast-notifications'
 
 function App() {
   return (
     <div className="App">
-        <Routes />
+        <ToastProvider 
+          placement="bottom-center"
+          autoDismissTimeout={6000}
+        >
+          <Routes />
+        </ToastProvider>
     </div>
   );
 }
