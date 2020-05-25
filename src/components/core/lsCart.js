b0b3de6e05e0e6663cc2164c6c9e1da05e5af2ba
Aleksandr YUrkovskiy <godlikedesigner@gmail.com>
2020-05-25 14:07:39 +0500

    fixed total price when ls cart save

--- src/components/core/lsCart.js
+++ src/components/core/lsCart.js
@@ -3,7 +3,7 @@ export const addFoodToCart = food => {
     cart.totalItems++;
     cart.items.push(food);
     cart.total = cart.items.reduce((prev, next) => {
-        return prev + next.total
+        return prev + next.totalPrice
     }, 0);
     localStorage.setItem('cart', JSON.stringify(cart));
 };
@@ -13,22 +13,22 @@ export const updateFoodCart = (id, action) => {
     const foodToUpdate = cart.items.find(f => f.uid === id);
     if(action === 'inc') {
         foodToUpdate.quantity += 1;
-        foodToUpdate.total =   foodToUpdate.price * foodToUpdate.quantity;
+        foodToUpdate.total = foodToUpdate.totalPrice * foodToUpdate.quantity;
     } else if(action === 'dec') {
         foodToUpdate.quantity = foodToUpdate.quantity === 1 ? 1 : foodToUpdate.quantity - 1;
-        foodToUpdate.total =   foodToUpdate.price * foodToUpdate.quantity;
+        foodToUpdate.total = foodToUpdate.totalPrice * foodToUpdate.quantity;
     } else {
         const itemIndex = cart.items.findIndex(({uid}) => uid === id);
         cart.items.splice(itemIndex, 1);
         cart.totalItems = cart.items.length;
     }
     cart.total = cart.items.reduce((prev, next) => {
-        return prev + next.total
+        return prev + next.totalPrice
     }, 0);
 
     console.log(
         cart.items.reduce((prev, next) => {
-            return prev + next.total
+            return prev + next.totalPrice
         }, 0)
     )
 
